1e3a50091898e4e980b1e5572e273fdbb2c58863
Kloster96 <luciano.kloster@hotmail.com>
2023-02-07 10:20:31 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://www.pnguniverse.com/wp-content/uploads/2021/04/Mujer-en-traje-de-bano-162b8549.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://pngimg.com/uploads/jacket/small/jacket_PNG8027.png",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://pngimg.com/uploads/socks/small/socks_PNG8267.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://assets.stickpng.com/thumbs/5c839e6672f5d9028c17eda6.png",
+    },
+    {
+      id:8,
+      img:"https://assets.stickpng.com/thumbs/58e38cd2204d556bbd97b15d.png",
+    },
+    {
+      id:9,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+    {
+      id:10,
+      img:"https://pngimg.com/uploads/boots/small/boots_PNG7809.png",
+    },
+    {
+      id:11,
+      img:"https://freepngimg.com/thumb/jeans/6-jeans-png-image.png",
+    },
+    {
+      id:12,
+      img:"https://freepngimg.com/thumb/shoes/27428-5-nike-shoes-transparent-background.png",
+    },
+  ]
